a23c4f3a2b0a147dc43f92b6e51166a7ca29c97a
Александр <160741752+Alexandch@users.noreply.github.com>
2024-03-13 21:03:16 +0300

    Update MockClubEventService.cs

--- Club58Bot/Club58Bot/MockClubEventService.cs
+++ Club58Bot/Club58Bot/MockClubEventService.cs
@@ -1,15 +1,13 @@
-﻿using Club58Bot;
-
-namespace Club58_Bot;
+namespace Club58Bot;
 
 public class MockClubEventService : IClubEventService
 {
-    public List<Event> GetEvents()
+    public IEnumerable<ClubEvent> GetEvents()
     {
-        return new List<Event>
-        {
-            new Event { Id = 1, Name = "Event 1" },
-            new Event { Id = 2, Name = "Event 2" }
-        };
+        return
+        [
+            new ClubEvent { Id = 1, Name = "Event 1" },
+            new ClubEvent { Id = 2, Name = "Event 2" }
+        ];
     }
 }
