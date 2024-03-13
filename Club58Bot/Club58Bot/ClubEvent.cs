062b8bd4ac6f31881bad3bfffb40ef7aca6f571e
Александр <160741752+Alexandch@users.noreply.github.com>
2024-03-13 21:02:29 +0300

    Update and rename Event.cs to ClubEvent.cs

---
+++ Club58Bot/Club58Bot/ClubEvent.cs
@@ -0,0 +1,7 @@
+namespace Club58Bot;
+
+public class ClubEvent
+{
+    public int Id { get; set; }
+    public string Name { get; init; } = null!;
+}
--- Club58Bot/Club58Bot/Event.cs
+++
@@ -1,7 +0,0 @@
-﻿namespace Club58Bot;
-
-public class Event
-{
-    public int Id { get; set; }
-    public string Name { get; set; }
-}
