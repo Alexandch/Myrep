4ea16f3111211706b8b654fb0adecb642395e53c
Sanek <chueshov007@gmail.com>
2024-03-28 20:02:32 +0300

    delete unnecessary comments

--- webhookbot/Club58BotWeb/Program.cs
+++ webhookbot/Club58BotWeb/Program.cs
@@ -11,11 +11,6 @@ builder.Services.Configure<BotConfiguration>(botConfigurationSection);
 
 var botConfiguration = botConfigurationSection.Get<BotConfiguration>();
 
-// Register named HttpClient to get benefits of IHttpClientFactory
-// and consume it with ITelegramBotClient typed client.
-// More read:
-//  https://docs.microsoft.com/en-us/aspnet/core/fundamentals/http-requests#typed-clients
-//  https://docs.microsoft.com/en-us/dotnet/architecture/microservices/implement-resilient-applications/use-httpclientfactory-to-implement-resilient-http-requests
 builder.Services.AddHttpClient("telegram_bot_client")
                 .AddTypedClient<ITelegramBotClient>((httpClient, sp) =>
                 {
@@ -27,15 +22,8 @@ builder.Services.AddHttpClient("telegram_bot_client")
 // Dummy business-logic service
 builder.Services.AddScoped<UpdateHandlers>();
 
-// There are several strategies for completing asynchronous tasks during startup.
-// Some of them could be found in this article https://andrewlock.net/running-async-tasks-on-app-startup-in-asp-net-core-part-1/
-// We are going to use IHostedService to add and later remove Webhook
 builder.Services.AddHostedService<ConfigureWebhook>();
 
-// The Telegram.Bot library heavily depends on Newtonsoft.Json library to deserialize
-// incoming webhook updates and send serialized responses back.
-// Read more about adding Newtonsoft.Json to ASP.NET Core pipeline:
-//   https://docs.microsoft.com/en-us/aspnet/core/web-api/advanced/formatting?view=aspnetcore-6.0#add-newtonsoftjson-based-json-format-support
 builder.Services
     .AddControllers()
     .AddNewtonsoftJson();
